cd86929dc063eefbd9400f699b4c68500d9b3c8c
Sandesh Athreya <sandesh@Sandeshs-MacBook-Pro.local>
2020-03-31 18:03:17 +0200

    initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url='https://api.darksky.net/forecast/147ee745e70f6e028dd551d892d2dca3/' + latitude + ',' + longitude +'?units=si'
+    request({url, json: true}, (error, {body}) => {
+        if(error) {
+            callback('Unable to connect to the API', undefined)
+        }else if(body.error === 0) {
+            callback('The request is broken or Invalid', undefined)
+        } else {
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees. There is a ${body.currently.precipProbability}% chance of rain`)
+        }
+    })
+}
+
+module.exports = forecast
